646001932bb85c2a1564a1c6be4c074c0cf9ffbb
Cduy11 <cduy527@gmail.com>
2024-09-25 11:57:47 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import BaiTap from './BaiTapThucHanhLayout/BaiTap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BaiTap/>
     </div>
   );
 }
---
+++ src/BaiTapThucHanhLayout/BaiTap.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './Banner'
+import Header from './Header'
+import Body from './Body'
+import Footer from './Footer'
+
+export default function BaiTap() {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+        <Body/>
+        <Footer/>
+    </div>
+  )
+}
